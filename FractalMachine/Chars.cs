98e4dd63f72119c23b97623af7f15150316bab37
Riccardo Cecchini <rcecchini.ds@gmail.com>
2020-07-01 17:43:46 +0200

    Ho fame

---
+++ FractalMachine/Chars.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FractalMachine
+{
+    public class CharType
+    {
+
+        #region Properties
+
+        public char Char { get; }
+        public TypeEnum Type { get; }
+        public CharTypeEnum CharacterType { get; } = CharTypeEnum.None;
+        public AlphaTypeEnum AlphaType { get; } = AlphaTypeEnum.None;
+        public SymbolBlockEnum SymbolBlock { get; } = SymbolBlockEnum.None;
+
+        public bool IsAlphanumeric
+        {
+            get
+            {
+                return CharacterType == CharTypeEnum.Alpha || CharacterType == CharTypeEnum.Numeric;
+            }
+        }
+
+        #endregion
+
+        public CharType(char Char)
+        {
+            // http://www.asciitable.com/
+            this.Char = Char;
+            int i = (int)Char;
+
+            // relax, re do it
+            Type = (i >= 32 && i < 127) ? TypeEnum.Char : TypeEnum.Signal;
+
+            if (Type == TypeEnum.Char)
+            {
+                if (i <= 57)
+                {
+                    if (i >= 48)
+                    {
+                        CharacterType = CharTypeEnum.Numeric;
+                    }
+                    else
+                    {
+                        CharacterType = CharTypeEnum.Symbol;
+                        SymbolBlock = SymbolBlockEnum.First;
+                    }
+
+                }
+                else if (i <= 64)
+                {
+                    CharacterType = CharTypeEnum.Symbol;
+                    SymbolBlock = SymbolBlockEnum.Second;
+                }
+                else if (i <= 122)
+                {
+                    CharacterType = CharTypeEnum.Alpha;
+
+                    if (i <= 96)
+                    {
+                        if (i >= 91)
+                        {
+                            CharacterType = CharTypeEnum.Symbol;
+                            SymbolBlock = SymbolBlockEnum.Third;
+                        }
+                        else
+                        {
+                            AlphaType = AlphaTypeEnum.Uppercase;
+                        }
+                    }
+                    else
+                    {
+                        AlphaType = AlphaTypeEnum.Lowercase;
+                    }
+                }
+                else
+                {
+                    CharacterType = CharTypeEnum.Symbol;
+                    SymbolBlock = SymbolBlockEnum.Fourth;
+                }
+            }
+        }
+
+        #region Enums
+
+        public enum TypeEnum
+        {
+            Signal,
+            Char
+        }
+
+        public enum CharTypeEnum
+        {
+            None,
+            Alpha,
+            Numeric,
+            Symbol
+        }
+
+        public enum AlphaTypeEnum
+        {
+            None,
+            Uppercase,
+            Lowercase
+        }
+
+        public enum SymbolBlockEnum
+        {
+            None = -1,
+            First,
+            Second,
+            Third,
+            Fourth
+        }
+
+        #endregion
+    }
+}
---
+++ FractalMachine/Light.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FractalMachine
+{
+    public class Light
+    {
+        #region Static
+
+        public static void OpenScript(string FileName)
+        {
+
+        }
+
+        #endregion
+
+        #region Parse
+
+        class WordsDispatcher
+        {
+            public class Word
+            {
+                public string String;
+                public bool IsAlphanumeric;
+            }
+
+            public delegate void OnDispatchDelegate(Word Word);
+            public OnDispatchDelegate OnDispatch;
+
+            public CharType prevCharType;
+            List<string> strings = new List<string>();
+            string curString = "";
+
+            public void Push(char Char)
+            {
+                var charType = new CharType(Char);
+
+                if(prevCharType != null)
+                {
+                    if(charType.IsAlphanumeric != prevCharType.IsAlphanumeric)
+                    {
+                        Flush();
+                    }
+                }
+
+                curString += Char;
+                prevCharType = charType;
+
+                // check for corrispondece
+            }
+
+            public void Flush()
+            {
+                var word = new Word();
+                word.String = curString;
+                word.IsAlphanumeric = prevCharType.IsAlphanumeric;
+
+                OnDispatch(word);
+                curString = "";
+            }
+
+        }
+
+        void Parse(string Script)
+        {
+            WordsDispatcher dispatcher = new WordsDispatcher();
+
+            dispatcher.OnDispatch = delegate (WordsDispatcher.Word Word)
+            {
+
+            };
+
+
+            ///
+            /// Cycle string
+            ///
+            foreach (char ch in Script)
+            {
+                dispatcher.Push(ch);
+            }
+
+            dispatcher.Flush();
+
+        }
+
+        #endregion
+    }
+}
