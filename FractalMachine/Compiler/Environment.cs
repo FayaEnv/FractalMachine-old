b9e6fa96a8448323fdcfa9509880abf8a8521e08
Riccardo Cecchini <rcecchini.ds@gmail.com>
2020-10-25 06:13:58 +0100

    Remake Environment
    Tutto un nuovo modo di vedere il mondo

--- FractalMachine/Compiler/Environment.cs
+++ FractalMachine/Compiler/Environment.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Text;
 
 namespace FractalMachine.Compiler
@@ -22,9 +24,70 @@ namespace FractalMachine.Compiler
 
         #endregion
 
+        PlatformID Platform;
+        public string Path = "/usr/bin/";
+
         public Environment()
         {
             var osVersion = System.Environment.OSVersion;
+            Platform = osVersion.Platform;
+
+            if (Platform == PlatformID.Win32NT)
+            {
+                if (!Directory.Exists("cygwin64-light"))
+                {
+
+                }
+
+                Path = "cygwin64-light" + Path;
+            }
+        }
+
+        public Command ExecuteCommand(string command)
+        {
+            var cmd = new Command(this);
+
+            cmd.Process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = Path,
+                    Arguments = $"",                    
+                    UseShellExecute = false,
+                    CreateNoWindow = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    RedirectStandardInput = true,
+                }
+            };
+
+            return cmd;
+        }
+    }
+
+    public class Command
+    {
+        public Process Process;
+        public Environment Environment;
+        public string[] OutLines, OutErrors;
+
+        public Command(Environment environment)
+        {
+            Environment = environment;
+        }
+
+        public void Run()
+        {
+            Process.Start();
+
+            while (!Process.HasExited) ;
+
+            List<string> lines = new List<string>(), err = new List<string>();
+            string s;
+            while ((s = Process.StandardOutput.ReadLine()) != null) lines.Add(s);
+            while ((s = Process.StandardError.ReadLine()) != null) err.Add(s);
+            OutLines = lines.ToArray();
+            OutErrors = lines.ToArray();
         }
     }
 }
