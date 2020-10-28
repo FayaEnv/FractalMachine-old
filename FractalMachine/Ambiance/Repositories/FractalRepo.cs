400caedd3283fe364f9087b43994d2d54e0f82b9
Riccardo Cecchini <rcecchini.ds@gmail.com>
2020-10-28 04:22:09 +0100

    Prima o poi lo dovevo pur fare.
    Corro troppo? Quando sarà davvero un'organizzazione mettero il suo nome al posto del mio

---
+++ FractalMachine/Ambiance/Repositories/FractalRepo.cs
@@ -0,0 +1,63 @@
+﻿/*
+   Copyright 2020 (c) Riccardo Cecchini
+   
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FractalMachine.Ambiance.Repositories
+{
+    /// <summary>
+    /// FractalRepo will be the only repository not linked to an Environment but to the Compiler
+    /// </summary>
+    public class FractalRepo : Repository
+    {
+        public FractalRepo(Environment Env) : base(Env)
+        {
+        }
+
+        public override void Search(string query)
+        {
+            //todo
+        }
+
+        public override void Info(string query)
+        {
+            //todo
+        }
+
+        public override InstallationResult Install(string Package, bool Depedency = false)
+        {
+            //todo
+            return InstallationResult.PackageNotFound;
+        }
+
+        public override void List(string query)
+        {
+            //todo
+        }
+
+        public override void Upgrade(string query)
+        {
+            //todo
+        }
+
+        public override void Update()
+        {
+            //todo
+        }
+    }
+}
--- FractalMachine/Classes/Debug.cs
+++ FractalMachine/Classes/Debug.cs
@@ -18,32 +18,13 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace FractalMachine.Ambiance.Compilers
+namespace FractalMachine
 {
-    public class MSVC : Compiler
+    class Debug
     {
-        public MSVC(Environment Env) : base(Env)
+        static public void Print(string str)
         {
-        }
-
-        public override void Compile(string FileName, string Out)
-        {
-            FileName = System.IO.Path.GetFullPath(FileName);
-            FileName = env.Path(FileName);
-            Out = env.Path(Out);
-
-            var cmd = env.NewCommand("g++");
-            //cmd.DirectCall = true;
-            cmd.UseStdWrapper = true;
-            cmd.AddArgument(FileName);
-            cmd.AddArgument("-o", Out);
-
-            // Std libs
-            cmd.AddArgument("-std=c++11");
-
-            cmd.Run();
-
-            string read = "";
+            Console.WriteLine(str);
         }
     }
 }
