57bc18fc50923311a3e00d32a7fcb3fd177e918e
Riccardo Cecchini <rcecchini.ds@gmail.com>
2020-11-12 20:05:07 +0100

    Ho già cambiato idea
    Cosa volete che vi dica, sono fatto così

---
+++ FractalMachine/Code/Components/Struct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FractalMachine.Code.Components
+{
+    public class Struct : Function
+    {
+        public Struct(Component parent, Linear linear) : base(parent, linear)
+        {
+            //containerType = ContainerTypes.Class;
+        }
+    }
+}
